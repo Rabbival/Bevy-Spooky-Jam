3eb041a2901a82fa287ee543b4bfce635a14db1c
David Romaní <david@flux.cat>
2024-10-06 10:14:30 +0200

    add MonsterSpawningSequenceManagerPlugin

--- src/game/monsters/mod.rs
+++ src/game/monsters/mod.rs
@@ -1,14 +1,17 @@
 use crate::prelude::*;
 use crate::prelude::monster_spawner::MonsterSpawnerPlugin;
+use crate::prelude::monster_spawning_sequence_manager::MonsterSpawningSequenceManagerPlugin;
 
 pub mod consts;
 pub mod monster_spawner;
 pub mod monster_state;
+pub mod monster_spawning_sequence_manager;
+pub mod monster_error;
 
 pub struct MonstersPlugin;
 
 impl Plugin for MonstersPlugin {
     fn build(&self, app: &mut App) {
-        app.add_plugins(MonsterSpawnerPlugin);
+        app.add_plugins((MonsterSpawnerPlugin, MonsterSpawningSequenceManagerPlugin));
     }
 }
