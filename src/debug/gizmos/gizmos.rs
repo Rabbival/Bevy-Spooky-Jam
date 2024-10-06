4912cf98b40fda7600ce44ec5d252078a3e2e3e2
David Romaní <david@flux.cat>
2024-10-06 13:30:16 +0200

    set Gizmo default line width config thinner

--- src/debug/gizmos/gizmos.rs
+++ src/debug/gizmos/gizmos.rs
@@ -4,8 +4,14 @@ pub struct GizmosPlugin;
 
 impl Plugin for GizmosPlugin {
     fn build(&self, app: &mut App) {
-        app.add_systems(Update, draw_monster_hearing_ring_system);
+        app.add_systems(Startup, config_line_width)
+            .add_systems(Update, draw_monster_hearing_ring_system);
     }
+}
+
+fn config_line_width(mut config_store: ResMut<GizmoConfigStore>) {
+    let (config, _) = config_store.config_mut::<DefaultGizmoConfigGroup>();
+    config.line_width = 0.3;
 }
 
 fn draw_monster_hearing_ring_system(
