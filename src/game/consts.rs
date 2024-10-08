9526088f4c3b1cee5db57bb04f7d951f72836872
David Romaní <david@flux.cat>
2024-10-08 17:58:00 +0200

    remove unnecessary constants from legacy code

--- src/game/consts.rs
+++ src/game/consts.rs
@@ -1,11 +1,3 @@
-pub const ORB_MAX_COUNT: usize = 3;
-pub const ORB_MAX_RADIUS: f32 = 42.0;
-pub const ORB_COLLECTION_TIME: f32 = 0.4;
-pub const ORB_COLLECTION_POWER: f32 = 2.0;
-
-pub const EXAMPLE_PATROLLER_SQUARE_DURATION: f32 = 1.0;
-pub const EXAMPLE_PATROLLER_DIAGON_DURATION: f32 = 2.0;
-
 pub const MULTIPLIER_WHEN_SLOW_MOTION: f32 = 0.05;
 pub const SLOW_MOTION_KICK_IN_AND_OUT_TIME: f32 = 0.2;
 
