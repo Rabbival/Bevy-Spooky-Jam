0db7888824030fbca9d7992942dd17cce3e38b6e
David Romaní <david@flux.cat>
2024-10-09 20:50:41 +0200

    add AssetsLoaderPlugin

---
+++ src/app/assets_loader.rs
@@ -0,0 +1,50 @@
+use crate::prelude::*;
+use bevy::prelude::*;
+
+#[derive(Resource, Clone, Default)]
+pub struct SpritesAtlas {
+    pub atlas_handle: Handle<TextureAtlasLayout>,
+    pub image_handle: Handle<Image>
+}
+
+#[derive(Resource, Deref, DerefMut, Clone, Default)]
+pub struct TextFonts {
+    pub kenny_blocks_handle: Handle<Font>,
+}
+
+pub struct AssetsLoaderPlugin;
+
+impl Plugin for AssetsLoaderPlugin {
+    fn build(&self, app: &mut App) {
+        app.add_systems(PreStartup, (
+            sprites_atlas_setup, 
+            text_font_setup
+        ));
+    }
+}
+
+fn sprites_atlas_setup(
+    asset_server: Res<AssetServer>,
+    mut texture_atlases: ResMut<Assets<TextureAtlasLayout>>,
+    mut commands: Commands,
+) {
+    let image_handle = asset_server.load("images/sprites_sheet.png");
+    let texture_atlas = TextureAtlasLayout::from_grid(
+        UVec2::new(40, 40),
+        3,
+        1,
+        None,
+        None,
+    );
+    let texture_atlas_handle = texture_atlases.add(texture_atlas);
+    commands.insert_resource(SpritesAtlas {
+        atlas_handle: texture_atlas_handle,
+        image_handle
+    });
+}
+
+fn text_font_setup(mut commands: Commands, asset_server: Res<AssetServer>) {
+    commands.insert_resource(TextFonts {
+        kenny_blocks_handle: asset_server.load("fonts/kenney_blocks.ttf")
+    });
+}
