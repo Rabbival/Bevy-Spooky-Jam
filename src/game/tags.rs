408e9496e007a955127b7b79621cd1f1d47e3157
David Romaní <david@flux.cat>
2024-10-09 16:19:23 +0200

    add WorldChampionshipLeaderboardScoring component

--- src/game/tags.rs
+++ src/game/tags.rs
@@ -1,3 +1,4 @@
+use std::time::Duration;
 use crate::prelude::*;
 
 use bevy::time::Stopwatch;
@@ -8,4 +9,10 @@ pub struct WorldBoundsWrapped;
 #[derive(Component, Default)]
 pub struct PlayerGameStopwatch {
     pub timer: Stopwatch,
+}
+
+#[derive(Component, Default)]
+pub struct WorldChampionshipLeaderboardScoring {
+    pub elapsed: Duration,
+    pub hi_score: u32,
 }
