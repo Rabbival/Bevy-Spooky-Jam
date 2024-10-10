e244559d0df4ff9dc3a8b605b9b9205e7bfbb3e4
EitanWix <eitanm@wix.com>
2024-10-10 09:59:47 +0300

    bombs color tweak

--- src/game/bombs/bomb_state.rs
+++ src/game/bombs/bomb_state.rs
@@ -1,5 +1,5 @@
 use bevy::color::{
-    palettes::css::{ANTIQUE_WHITE, DARK_RED, DARK_SLATE_GRAY},
+    palettes::css::{ANTIQUE_WHITE, DARK_ORANGE, DARK_RED, DARK_SLATE_GRAY, WHITE},
     Color, Srgba,
 };
 
@@ -19,11 +19,11 @@ impl BombState {
     pub fn to_colors(&self) -> Option<BombAndTextColors> {
         match self {
             Self::PreHeld => Some(BombAndTextColors {
-                bomb: Color::from(DARK_RED),
+                bomb: Color::from(DARK_ORANGE),
                 text: Color::from(DARK_SLATE_GRAY),
             }),
             Self::Held => Some(BombAndTextColors {
-                bomb: Color::from(ANTIQUE_WHITE),
+                bomb: Color::from(WHITE),
                 text: Color::from(Srgba::new(0.0, 0.0, 0.0, 1.0)),
             }),
             Self::PostHeld => None,
