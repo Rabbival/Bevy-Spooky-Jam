947513335d794c8b86c9b4e1e2cc96a109e065bd
EitanWix <eitanm@wix.com>
2024-10-16 13:17:35 +0300

    add camera shake

--- src/app/consts.rs
+++ src/app/consts.rs
@@ -3,3 +3,4 @@ use crate::prelude::*;
 pub const WINDOW_SIZE_IN_PIXELS: f32 = 800.0;
 pub const SCREEN_COLOR_BACKGROUND: ClearColor = ClearColor(Color::srgb(0.1, 0.1, 0.1));
 pub const TOP_UI_HEADER_BAR_HEIGHT: f32 = 60.0;
+pub const CAMERA_SHAKE_DURATION: f32 = 0.1;
