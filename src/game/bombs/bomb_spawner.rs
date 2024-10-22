a1a104faf05c0e796f83a62e8816c8dd2cd16f60
EitanWix <eitanm@wix.com>
2024-10-22 06:59:28 +0300

    add bombs max ciel

--- src/game/bombs/bomb_spawner.rs
+++ src/game/bombs/bomb_spawner.rs
@@ -27,6 +27,7 @@ fn respawn_initial_bomb_on_game_restart(
     timer_fire_request_writer: EventWriter<TimerFireRequest>,
     transforms_not_to_spawn_next_to: Query<&Transform, Or<(With<Player>, With<Bomb>)>>,
     sprites_atlas_resource: ResMut<SpritesAtlas>,
+    bombs_query: Query<&Bomb>,
     commands: Commands,
 ) {
     if read_no_field_variant!(event_reader, GameEvent::RestartGame).count() > 0 {
@@ -34,6 +35,7 @@ fn respawn_initial_bomb_on_game_restart(
             timer_fire_request_writer,
             transforms_not_to_spawn_next_to,
             sprites_atlas_resource,
+            bombs_query,
             commands,
         );
     }
@@ -43,12 +45,14 @@ fn spawn_initial_bombs(
     mut timer_fire_request_writer: EventWriter<TimerFireRequest>,
     transforms_not_to_spawn_next_to: Query<&Transform, Or<(With<Player>, With<Bomb>)>>,
     mut sprites_atlas_resource: ResMut<SpritesAtlas>,
+    bombs_query: Query<&Bomb>,
     mut commands: Commands,
 ) {
     if let Err(bomb_error) = try_spawning_a_bomb(
         &mut timer_fire_request_writer,
         &transforms_not_to_spawn_next_to,
         &mut sprites_atlas_resource,
+        &bombs_query,
         &mut commands,
     ) {
         print_warning(bomb_error, vec![LogCategory::RequestNotFulfilled]);
@@ -60,6 +64,7 @@ fn listen_for_bomb_spawning_requests(
     mut timer_fire_request_writer: EventWriter<TimerFireRequest>,
     transforms_not_to_spawn_next_to: Query<&Transform, Or<(With<Player>, With<Bomb>)>>,
     mut sprites_atlas_resource: ResMut<SpritesAtlas>,
+    bombs_query: Query<&Bomb>,
     mut commands: Commands,
 ) {
     for done_event in timer_done_event_reader.read() {
@@ -68,6 +73,7 @@ fn listen_for_bomb_spawning_requests(
                 &mut timer_fire_request_writer,
                 &transforms_not_to_spawn_next_to,
                 &mut sprites_atlas_resource,
+                &bombs_query,
                 &mut commands,
             ) {
                 print_warning(bomb_error, vec![LogCategory::RequestNotFulfilled]);
@@ -80,8 +86,12 @@ fn try_spawning_a_bomb(
     timer_fire_request_writer: &mut EventWriter<TimerFireRequest>,
     transforms_not_to_spawn_next_to: &Query<&Transform, Or<(With<Player>, With<Bomb>)>>,
     sprites_atlas_resource: &mut ResMut<SpritesAtlas>,
+    bombs_query: &Query<&Bomb>,
     commands: &mut Commands,
 ) -> Result<(), BombError> {
+    if bombs_query.iter().count() >= MAX_BOMB_COUNT {
+        return Ok(());
+    }
     let place_to_spawn_in = try_finding_place_for_bomb(transforms_not_to_spawn_next_to)?;
     let bomb_component = Bomb::default();
     let newborn_bomb = commands
