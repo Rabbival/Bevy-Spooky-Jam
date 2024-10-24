71799616cbce252dd7e4fb109d6adab9f2f754c2
EitanWix <eitanm@wix.com>
2024-10-24 18:31:08 +0300

    reset game timer at the right time

--- src/game/scores/score_manager.rs
+++ src/game/scores/score_manager.rs
@@ -1,5 +1,6 @@
 use crate::prelude::*;
 use crate::read_no_field_variant;
+use crate::single_else_return;
 use crate::single_mut_else_return;
 
 pub struct ScoreManagerPlugin;
@@ -8,7 +9,10 @@ impl Plugin for ScoreManagerPlugin {
     fn build(&self, app: &mut App) {
         app.add_systems(Startup, spawn_score_entities).add_systems(
             Update,
-            (update_player_scoring, listen_for_game_over_events).chain(),
+            (
+                (update_player_scoring, listen_for_game_over_events).chain(),
+                listen_for_restart_events.in_set(GameRestartSystemSet::Spawning),
+            ),
         );
     }
 }
@@ -34,13 +38,13 @@ fn listen_for_game_over_events(
     mut game_score_query: Query<&mut CurrentGameScore>,
     mut best_score_query: Query<&mut BestScoreSoFar>,
     mut longest_survived_query: Query<&mut LongestSurvivedSoFar>,
-    mut stopwatch_query: Query<&mut PlayerGameStopwatchUi>,
+    stopwatch_query: Query<&PlayerGameStopwatchUi>,
 ) {
     if read_no_field_variant!(game_events_listener, GameEvent::GameOver).count() > 0 {
         let mut game_score = single_mut_else_return!(game_score_query);
         let mut best_score = single_mut_else_return!(best_score_query);
         let mut longest_survived = single_mut_else_return!(longest_survived_query);
-        let mut stopwatch = single_mut_else_return!(stopwatch_query);
+        let stopwatch = single_else_return!(stopwatch_query);
         if game_score.0 > best_score.0 {
             best_score.0 = game_score.0;
         }
@@ -48,6 +52,14 @@ fn listen_for_game_over_events(
         if stopwatch.timer.elapsed_secs() > longest_survived.0 {
             longest_survived.0 = stopwatch.timer.elapsed_secs();
         }
-        stopwatch.timer.reset();
+    }
+}
+
+fn listen_for_restart_events(
+    mut game_events_listener: EventReader<GameEvent>,
+    mut stopwatch_query: Query<&mut PlayerGameStopwatchUi>,
+) {
+    if read_no_field_variant!(game_events_listener, GameEvent::RestartGame).count() > 0 {
+        single_mut_else_return!(stopwatch_query).timer.reset();
     }
 }
