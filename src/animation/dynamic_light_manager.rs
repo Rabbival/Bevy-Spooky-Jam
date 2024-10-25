e055761a3b6b1b65afaf929a87e48d5ad3ac526e
EitanWix <eitanm@wix.com>
2024-10-25 21:09:12 +0300

    not sure it works

--- src/animation/dynamic_light_manager.rs
+++ src/animation/dynamic_light_manager.rs
@@ -1,4 +1,4 @@
-use crate::prelude::*;
+use crate::{prelude::*, read_no_field_variant};
 use bevy_light_2d::light::PointLight2d;
 
 pub struct DynamicLightManagerPlugin;
@@ -6,7 +6,10 @@ impl Plugin for DynamicLightManagerPlugin {
     fn build(&self, app: &mut App) {
         app.add_systems(
             Update,
-            set_light_intensity.in_set(TickingSystemSet::PostTicking),
+            (
+                set_light_intensity.in_set(TickingSystemSet::PostTicking),
+                fade_light_on_game_over,
+            ),
         );
     }
 }
@@ -32,4 +35,43 @@ fn set_light_intensity(
             }
         }
     }
+}
+
+fn fade_light_on_game_over(
+    mut game_over_listener: EventReader<GameEvent>,
+    mut timer_fire_writer: EventWriter<TimerFireRequest>,
+    lights: Query<(&PointLight2d, Entity)>,
+    mut commands: Commands,
+) {
+    for _game_over in read_no_field_variant!(game_over_listener, GameEvent::GameOver) {
+        for (light, entity) in &lights {
+            let calculator = light_fade_upon_game_over_calculator(light.intensity, &mut commands);
+            timer_fire_writer.send(TimerFireRequest {
+                timer: EmittingTimer::new(
+                    vec![TimerAffectedEntity {
+                        affected_entity: entity,
+                        value_calculator_entity: Some(calculator),
+                    }],
+                    vec![TimeMultiplierId::RealTime],
+                    AGAIN_SCREEN_FADE_TIME / 2.0,
+                    TimerDoneEventType::Nothing,
+                ),
+                parent_sequence: None,
+            });
+        }
+    }
+}
+
+fn light_fade_upon_game_over_calculator(current_intensity: f32, commands: &mut Commands) -> Entity {
+    commands
+        .spawn(GoingEventValueCalculator::new(
+            TimerCalculatorSetPolicy::IgnoreNewIfAssigned,
+            ValueByInterpolation::from_goal_and_current(
+                current_intensity,
+                0.0,
+                Interpolator::new(0.1),
+            ),
+            TimerGoingEventType::SetLightIntensity,
+        ))
+        .id()
 }
